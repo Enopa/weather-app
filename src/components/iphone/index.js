ad62f0d762f024fd624a33ef37ec138db1a489ec
Jamal <ec20548@qmul.ac.uk>
2022-03-21 22:54:28 +0000

    Added Comments

--- src/components/iphone/index.js
+++ src/components/iphone/index.js
@@ -27,6 +27,7 @@ export default class Iphone extends Component {
 	// a call to fetch weather data via wunderground
 	fetchWeatherData = (location) => {
 		// API URL with a structure of : ttp://api.wunderground.com/api/key/feature/q/country-code/city.json
+		//Edited this from the boiler plate to ensure that an error window is shown if the user inputs an incorrect city
 		var url = "http://api.openweathermap.org/data/2.5/weather?q=" + location + "&units=metric&APPID=3ade289044e1debf629af13a4c323f84";
 		$.ajax({
 			url: url,
@@ -34,13 +35,11 @@ export default class Iphone extends Component {
 			success : this.parseResponse,
 			error : function(req, err){ console.log('API call failed ' + err); alert("Not a valid city");}
 		})
-
-		// once the data grabbed, hide the button
-		
-
-		
 	}
 
+
+	//Displays the first screen the user will see when they enter the app
+	//An input field as well as instructions so it is clear what the user must enter
 	inputDisplay() 
 	{
 		return(
@@ -56,6 +55,7 @@ export default class Iphone extends Component {
 		
 	}
 
+	//Displays the custom buddy class
 	renderBuddy() {
 		return (
 			<Buddy className='buddyinfo' buddyInfo={this.state.buddyInfo}/> 
@@ -66,9 +66,12 @@ export default class Iphone extends Component {
 	render() {
 		// check if temperature data is fetched, if so add the sign styling to the page
 		const tempStyles = this.state.temp ? `${style.temperature} ${style.filled}` : style.temperature;
+		//Date retrieval and formatting
 		const d = new Date();
 		var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 		const date = d.toLocaleDateString("en-US", options);
+		//Temperature converted into a percentage so that it can be used with the radial temperature bar
+		//Highest temperature is 35, this is way above average and usually indicates very hot weather
 		const percentage = (this.state.temp / 35) * 100;
 
 		// display all weather data
@@ -78,14 +81,10 @@ export default class Iphone extends Component {
 				    {this.state.display ? null : <img class={style.buddypic} src="buddy.png" alt="Square Smiley Face" width="100"/>}
 				    <div class={style.buddy}>{this.state.temp != 0  ? this.renderBuddy() : null }</div>
 					{this.state.display ? null : <img class={style.icon} src={this.state.imageIcon} alt="Icon" width="80"/>}
-					<div class={style.date}>{this.state.display ? null : date}</div>
-					 
-					
+					<div class={style.date}>{this.state.display ? null : date}</div>					
 					{this.state.display ? null : <div class={ style.city }> { this.state.locate }</div>}
 					{this.state.display ? null : <button class={style.switchButton} onClick={() => location.reload()}>Switch Location</button>}
 					<div class={ style.conditions }>{ this.state.cond }</div>
-
-
 					{this.state.display ? null : <div class={style.circle} style={{width: 250}}><CircularProgressbar styles={buildStyles({pathColor: this.state.tempColor, textColor:'#252525'})} value={percentage} text={`${this.state.temp + '°'}`}/></div>}
 					<div class={ style.percent }>{ this.state.display ? null : this.state.cloudy + "%"}</div>
 					{this.state.display ? null : <img class={style.drop} src="rainDrop.png" alt="Rain Drop Icon" width="25"/>}
@@ -103,7 +102,9 @@ export default class Iphone extends Component {
 	}
 
 	parseResponse = (parsed_json) => {
+		//we had to retrieve a few extra variables from the json file
 		var location = parsed_json['name'];
+		//Temperature rounded to 1 to make it easier to read for the user
 		var temp_c = Math.round(parsed_json['main']['temp']);
 		var conditions = parsed_json['weather']['0']['description'];
 		var wind = parsed_json['wind']['speed'];
@@ -111,6 +112,7 @@ export default class Iphone extends Component {
 		var icon = parsed_json['weather']['0']['icon'];
 		var barColour = "#375f00";
 
+		//Initialising variables for our canvas
 		const canvas = document.getElementById('Canvas')
 		const ctx = canvas.getContext('2d')
 		canvas.height = innerHeight
@@ -120,6 +122,7 @@ export default class Iphone extends Component {
         
         
 		//Checking certain information to display for the buddy info
+		//Will be pushed to an array which the user can cycle through 
 		var b_info = [];
 		if (temp_c > 25)
 		{
@@ -152,6 +155,7 @@ export default class Iphone extends Component {
 
 		}
 
+		//If there are no unique weather conditions, the buddy will display one general thing
 		if (b_info.length == 0)
 		{
 			b_info.push("Looks like the weather is acting fairly normal today, have a great day!");	
@@ -166,11 +170,15 @@ export default class Iphone extends Component {
 			w_speed: wind,
 			buddyInfo: b_info,
 			cloudy: clouds,
+			//The API allowed us to grab icons for the weather, of which we utilised
 			imageIcon: "http://openweathermap.org/img/wn/" + icon + "@2x.png",
 			tempColor: barColour,
+			//Display set to false in parse response instead, to ensure the API call succeeded
 			display: false
 		});   
 
+		//Creating the wave and animating it
+		//Speed is affected by the wind speed
 		waveColour.push((waveColour[0]+waveColour[1])/2);
 
 		function animate()  { 
